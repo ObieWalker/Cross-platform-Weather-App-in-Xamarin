8d3b9bbc33e30282e5fe737e7f1bee8ffe40c080
ObieWalker <obinnawalker@gmail.com>
2017-10-31 08:46:28 +0100

    commit omitting packages

---
+++ WeatherWeather/WeatherWeather/ServiceHandler/WeatherServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherWeather.Models;
+using WeatherWeather.WeatherRestClient;
+
+namespace WeatherWeather.ServiceHandler
+{
+    public class WeatherServices
+    {
+        OpenWeatherMap<WeatherMainModel> _openWeatherRest = new OpenWeatherMap<WeatherMainModel>();
+        public async Task<WeatherMainModel> GetWeatherDetails(string city)
+        {
+            var getWeatherDetails = await _openWeatherRest.GetAllWeathers(city);
+            return getWeatherDetails;
+        }
+    }
+}
---
+++ WeatherWeather/WeatherWeather/WeatherRestClient/OpenWeatherMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace WeatherWeather.WeatherRestClient
+{
+    public class OpenWeatherMap<T>
+    {
+        private const string OpenWeatherApi = "http://api.openweathermap.org/data/2.5/weather?q=";
+        private const string Key = "c4c4819f8f77e37dd84ba7bc65ac0a1c";
+        HttpClient _httpClient = new HttpClient();
+
+        public async Task<T> GetAllWeathers(string city)
+        {
+            var json = await _httpClient.GetStringAsync(OpenWeatherApi + city + "&APPID=" + Key);
+            var getWeatherModels = JsonConvert.DeserializeObject<T>(json);
+            return getWeatherModels;
+        }
+    }
+}
